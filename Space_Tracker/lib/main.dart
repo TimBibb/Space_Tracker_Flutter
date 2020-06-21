9b28ab5b2fa6570f403f1d2fc9120bc83c5a99a7
TimBibb <timothybibbee@gmail.com>
2020-06-21 18:43:05 -0400

    tab bar

--- Space_Tracker/lib/main.dart
+++ Space_Tracker/lib/main.dart
@@ -32,8 +32,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-	MyHomePage({Key key, this.title}) : super(key: key);
-
+	MyHomePage({this.title});
+	
 	// This widget is the home page of your application. It is stateful, meaning
 	// that it has a State object (defined below) that contains fields that affect
 	// how it looks.
@@ -42,75 +42,70 @@ class MyHomePage extends StatefulWidget {
 	// case the title) provided by the parent (in this case the App widget) and
 	// used by the build method of the State. Fields in a Widget subclass are
 	// always marked "final".
-
+	@override 
+	_SatellitePageState createState() => _SatellitePageState();
 	final String title;
-
-	@override
-	_MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-	int _counter = 0;
-
-	void _incrementCounter() {
-		setState(() {
-			// This call to setState tells the Flutter framework that something has
-			// changed in this State, which causes it to rerun the build method below
-			// so that the display can reflect the updated values. If we changed
-			// _counter without calling setState(), then the build method would not be
-			// called again, and so nothing would appear to happen.
-			_counter += 4;
-		});
-	}
-	
+class _SatellitePageState extends State<MyHomePage> {
+	final List<Tab> myTabs = <Tab>[
+		Tab(text: 'Satellites'),
+		Tab(text: 'Launches'),
+  	];
+	  
 	@override
 	Widget build(BuildContext context) {
-		// This method is rerun every time setState is called, for instance as done
-		// by the _incrementCounter method above.
-		//
-		// The Flutter framework has been optimized to make rerunning build methods
-		// fast, so that you can just rebuild anything that needs updating rather
-		// than having to individually change instances of widgets.
-		return Scaffold(
+		return DefaultTabController(
+		length: myTabs.length,
+		child: Scaffold(
 			appBar: AppBar(
-				// Here we take the value from the MyHomePage object that was created by
-				// the App.build method, and use it to set our appbar title.
-				title: Text(widget.title),
+			bottom: TabBar(
+				tabs: myTabs,
 			),
-			body: Center(
-				// Center is a layout widget. It takes a single child and positions it
-				// in the middle of the parent.
-				child: GridView.count(
-						primary: true,
-						padding: const EdgeInsets.all(20),
-						crossAxisSpacing: 10,
-						mainAxisSpacing: 10,
-						crossAxisCount: 3,
-						
-						children: <Widget>[
-							new Container(
-							padding: const EdgeInsets.all(8),
-							color: Colors.blue,
-							child: new Center(
-								child: const Text(
-									'ISS',
-									textAlign: TextAlign.center,
-									style: TextStyle(
-											fontSize: 24,
-											fontWeight: FontWeight.bold,
-											color: Colors.white
-										),
-									),
-								),
-						)
-					],
-				)
 			),
-			floatingActionButton: FloatingActionButton(
-				onPressed: _incrementCounter,
-				tooltip: 'Increment',
-				child: Icon(Icons.add),
-			), // This trailing comma makes auto-formatting nicer for build methods.
+			body: TabBarView(
+			children: myTabs.map((Tab tab) {
+				final String label = tab.text.toLowerCase();
+				if (label == "satellites"){
+					return Scaffold(
+						body: Center(
+							// Center is a layout widget. It takes a single child and positions it
+							// in the middle of the parent.
+							child: GridView.count(
+									primary: true,
+									padding: const EdgeInsets.all(20),
+									crossAxisSpacing: 10,
+									mainAxisSpacing: 10,
+									crossAxisCount: 3,
+									
+									children: List.generate(50, (index){
+										return new FlatButton(
+											onPressed: (){},
+											padding: const EdgeInsets.all(8),
+											color: Colors.blue,
+											child: new Center(
+												child: const Text(
+													'ISS',
+													textAlign: TextAlign.center,
+													style: TextStyle(
+															fontSize: 24,
+															fontWeight: FontWeight.bold,
+															color: Colors.white
+													),
+												),
+											),
+										);
+									},
+								)
+							),
+						),
+					);
+				}else if (label == "launches"){
+
+				}
+			}).toList(),
+			),
+		),
 		);
 	}
 }
