2345c137e8d05b27ad179352752eb70e9cc272fd
Tim Bibbee <timothybibbee@gmail.com>
2020-06-27 23:53:50 -0400

    ui changes2

--- Space_Tracker/lib/main.dart
+++ Space_Tracker/lib/main.dart
@@ -129,10 +129,9 @@ class QuickHelpItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container (
 			decoration: BoxDecoration(
-    		borderRadius: BorderRadius.circular(10.0)
+    		borderRadius: BorderRadius.circular(10.0),
+				color: Colors.blue,
     	),
-    	padding: const EdgeInsets.all(10),
-			color: Colors.blue,
     	child: Align(
     		alignment: Alignment.center,
     		child: Text(title, style: TextStyle(fontSize: 24, color: Colors.white)),
