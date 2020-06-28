eeda800db1bed9b30416a90a5b6ecc521a6a06b2
Tim Bibbee <timothybibbee@gmail.com>
2020-06-27 22:41:12 -0400

    sample background stuff

--- Space_Tracker/lib/main.dart
+++ Space_Tracker/lib/main.dart
@@ -165,23 +165,21 @@ class MapWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
-			body: Builder(
-				builder: (context) => 
-					Align(
-						alignment: Alignment.bottomRight,
-						child: FloatingActionButton(
-							onPressed: () {
-								showBottomSheet(
-									context: context,
-									builder: (context) => Container(
-										color: Colors.red,
-									)
-								);
-							},
-						),
-					),
-			)
-		);
+			body: GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 2,
+        // Generate 100 widgets that display their index in the List.
+        children: List.generate(100, (index) {
+          return Center(
+            child: Text(
+              'ITEM ITEM ITEM',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+           );
+      	}),
+      ),
+    );
   }
 }
 
